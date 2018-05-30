b4b1402b1b31822067a0693e45a36e0619d0f230
gunabharathit <gunabharathi98@gmail.com>
2018-05-30 12:30:49 +0530

    if else

---
+++ src/com/basic/hlo.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class hlo {
+
+	public static void main(String[]  args)
+		{
+			int money=50;
+			if(money<=65)
+			{
+				System.out.println("age is less than or equal to 65");
+				}
+				else {
+					System.out.println("age is greater than 65");
+			}
+				
+		
+
+}}
