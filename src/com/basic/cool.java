ae5fcf5c98249ea17cacaafe545594e45d4a7516
gunabharathit <gunabharathi98@gmail.com>
2018-05-30 14:23:01 +0530

    ternary op

---
+++ src/com/basic/cool.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class cool {
+	public static void main(String args[])
+	{
+		int price=100,b;
+			int	a=20;
+		
+
+System.out.println(b =price<a ? 80 : 100);
+
+	
+		
+		
+		
+	}
+
+}
