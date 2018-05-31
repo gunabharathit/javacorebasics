d7653adbc1d3923d67b59cd9c3d40a86b4cac381
gunabharathit <gunabharathi98@gmail.com>
2018-05-31 13:04:13 +0530

    constructor

---
+++ src/com/basic/Date1.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class Date1 {
+	public static void main(String[]args)
+	{
+		Date d1=new Date(1,2017,8);
+		d1.displayDate();
+	}
+
+}
---
+++ src/com/basic/Invoice.java
@@ -0,0 +1,50 @@
+package com.basic;
+
+public class Invoice {
+	String partNumber;
+	String description;
+	int quantity;
+	double price;
+	public String getPartNumber() {
+		return partNumber;
+	}
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+	
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		if(quantity<0)
+			{
+			this.quantity=0;
+			}
+		else {
+	this.quantity = quantity;
+	}}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getprice(){
+		return price;
+	}
+	public void setprice(double price) {
+		if(price<0.0)
+		{
+			this.price=0.0;
+		}
+		else
+		{
+			this.price=price;
+		}}
+}
+
