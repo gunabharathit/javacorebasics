f74c7bddabd79e8e338d0815baf360a8d7b317e2
gunabharathit <gunabharathi98@gmail.com>
2018-06-01 21:26:48 +0530

    reverse prgm

--- src/com/basic/Array.java
+++ src/com/basic/Array.java
@@ -3,22 +3,17 @@ package com.basic;
 public class Array {
 	public static void main(String[] args)
 	{ 
-		int n=7899;
-		int t=n;
-		int a[]= new int[10];
-		int i=0;
-		while(t>0) {
-		int	r=t%10;
-			 a[i]=r; 
-			 i++;
-			t=t/10;
-		}
-			for(i=0; i<10; i++)
-			{
-			   System.out.println(a[i]);
-			}       
-		   }
-		}
+		int myarray[]= {3,4,5,2,33,24,2};
+	for(int counter=myarray.length-1;counter>=0;counter--) {
+		System.out.println(myarray[counter]);
+		
+		
+	}
+	}
+}
+
+
+	
 
 
 
